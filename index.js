9d7186d36724650cae7089497bffad84563a710d
KrishnaVaishnav98 <krishnavaishnav125@gmail.com>
2023-08-08 21:00:02 +0530

    crash check

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import { GitHubCalendar } from 'react-github-calendar';
-import React from 'react';
-import { GitHubCalendar } from 'react-github-calendar';
+// import { GitHubCalendar } from 'react-github-calendar';
+// import React from 'react';
+// import { GitHubCalendar } from 'react-github-calendar';
 
-const App = () => {
-    return (
-        <div>
-            <h1>GitHub Activity Calendar</h1>
-            <GitHubCalendar username="KrishnaVaishnav98" />
-        </div>
-    );
-};
+// const App = () => {
+//     return (
+//         <div>
+//             <h1>GitHub Activity Calendar</h1>
+//             <GitHubCalendar username="KrishnaVaishnav98" />
+//         </div>
+//     );
+// };
 
-export default App;
+// export default App;
 
